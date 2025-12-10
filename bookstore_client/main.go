dc642928a67abe7a8bafb60928c1f98e84acedbc
ming <756131502@qq.com>
2025-12-10 22:20:22 +0800

    bookstore_client实现

---
+++ bookstore_client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bookstore_client/pb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// 创建客户端
+	conn, err := grpc.NewClient("127.0.0.1:8091", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Printf("grpc.NewClient 127.0.0.1:8091 failed, err:%v\n", err)
+		return
+	}
+	defer conn.Close()
+	c := pb.NewBookstoreClient(conn)
+	res, err := c.ListBooks(context.Background(), &pb.ListBooksRequest{Shelf: 4})
+	if err != nil {
+		fmt.Printf("c.ListBooks failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("next_page_token:%v\n", res.NextPageToken)
+	for i, book := range res.Book {
+		fmt.Printf("%d:%#v\n", i, book)
+	}
+}
